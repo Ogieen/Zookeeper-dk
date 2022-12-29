dfad007da93489b24706b23c5b254b64a300275e
Ogieen <kasper610@gmail.com>
2022-12-29 13:45:58 +0100

    Initial Commit

---
+++ Zookeeper dk/Zookeeper_dkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zookeeper_dkApp.swift
+//  Zookeeper dk
+//
+//  Created by Damian Kasperowicz on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Zookeeper_dkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
